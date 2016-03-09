8be861d5d1027a6de3e70bc6b0300c6214758850
Tim <timdelgado@gamestop.com>
2016-03-09 14:35:46 -0600

    Initial Checkin

---
+++ WS_Fed_Sample/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WS_Fed_Sample
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WS_Fed_Sample/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Web.Mvc;
+
+namespace WS_Fed_Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.ClaimsIdentity = Thread.CurrentPrincipal.Identity;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
